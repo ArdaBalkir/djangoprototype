ac942b3b4e180399120e0e042e0271960d4cff80
Arda <abalkir2014@gmail.com>
2022-08-29 10:09:31 +0100

    payment button

--- internationalmarket/store/views.py
+++ internationalmarket/store/views.py
@@ -27,8 +27,7 @@ def checkout(request):
         order, created = Order.objects.get_or_create(customer=customer, closed=False)
         items = order.orderitem_set.all()
     else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        return redirect('/accounts/google/login/')
 
     context = {'order': order}
     return render(request, 'store/checkout.html', context)
